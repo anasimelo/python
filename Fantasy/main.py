1c955882dbbbd0bc904f99c18a23cfb213c54c79
Aluno <aluno@gmail.com>
2025-08-06 17:22:33 -0300

    aula de hj

--- Fantasy/main.py
+++ Fantasy/main.py
@@ -1,10 +1,49 @@
 def main():
     inicio = int (input("Digite seu inicio do intervalo: "))
     fim = int (input("Digite o fim do intervalo: "))
-    i = 10
-    j = 40
 
-    while i < j:
-        print()
+    primos = "" 
+
+
+    if inicio < fim:
+      while inicio <= fim:
+        i = 2
+
+        while inicio > i:
+           if inicio % i == 0:
+            break
+           i += 1
+
+        if inicio == i and inicio > 1:
+           primos += "\n" + str(inicio)
+
+        inicio += 1
+    elif inicio > fim:
+        while inicio >= fim:
+           
+           i = 2
+
+           while inicio != i and inicio > 1:
+              if inicio % i == 0:
+                 break
+              i += 1
+
+        if inicio == i and inicio > 1:
+                primos += "\n" + str (inicio)
+        inicio -= 1
+    else:
+        i = 2
+
+        while inicio != i and inicio > 1:
+           if inicio % i == 0:
+              break
+        i += 1
+              
+    if inicio == i and inicio > 1:
+           primos += "\n" + str (inicio)
+    if primos == "":
+            primos += "intervalo sem primo"
+
+    print("Primos: ", primos)
     return 0
 main()
