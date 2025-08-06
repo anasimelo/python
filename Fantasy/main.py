8b01878b50c7898a14cc51e35e875538278e4ba0
Aluno <aluno@gmail.com>
2025-08-06 15:51:40 -0300

    aulas de hoje

---
+++ Fantasy/main.py
@@ -0,0 +1,10 @@
+def main():
+    inicio = int (input("Digite seu inicio do intervalo: "))
+    fim = int (input("Digite o fim do intervalo: "))
+    i = 10
+    j = 40
+
+    while i < j:
+        print()
+    return 0
+main()
---
+++ Tabuada/main.py
@@ -0,0 +1,27 @@
+def main():
+    inicio = int (input("Digite seu inicio do intervalo: "))
+    fim = int (input("Digite o fim do intervalo: "))
+
+
+    i = 1
+
+    if inicio < fim:
+        while inicio <= fim:
+            print("Tabuada do ", inicio)
+            i = 1
+            while i <= 10:
+                print( inicio, " X ", i, " = ", inicio * i)
+                i+= 1
+            print("\n")
+            inicio += 1
+    elif inicio > fim:
+        while inicio >= fim:
+            print("Tabuada do ", inicio)
+            i = 1
+            while i <= 10:
+                print( inicio, " X " , i, " = ", inicio * i)
+                i+= 1
+            print("\n")
+            inicio -= 1
+    return 0
+main()
